6285ee6c7462654fdbbf49da401dda7f5285eba1
Lucas <lucas.rabaquim@etec.sp.gov.br>
2023-03-23 15:21:07 -0300

    commit

---
+++ LucasRogerio1/Controllers/AulaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeLucasRogerio1.Controllers
+{
+    public class AulaController : Controller
+    {
+        // GET: Aula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aluno()
+        {
+            ViewBag.msg = "Os melhores alunos são aqueles que estudam mais!";
+            return View();
+        }
+
+        public ActionResult RedirectGoogle()
+        {
+            return Redirect("https://www.google.com.br/");
+        }
+    }
+}
